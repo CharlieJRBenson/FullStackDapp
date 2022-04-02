8f50c2a934a9ffdd66cdf9c246504a8ca5e8fe9f
Charlie Benson <chaza433@gmail.com>
2022-04-02 05:24:18 +0100

    first commit

---
+++ react-dapp/src/App.js
@@ -0,0 +1,64 @@
+import "./App.css";
+import { useState } from "react";
+import { ethers } from "ethers";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+function App() {
+  //store greeting locally
+  const [greeting, setGreetingValue] = useState();
+
+  //request metamask access
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  }
+
+  //call contract and read greeting val
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
+      try {
+        //call contract greet func and store returned val
+        const data = await contract.greet();
+        console.log("data : ", data);
+      } catch (error) {
+        console.log("error : ", error);
+      }
+    }
+  }
+
+  //call contract to set greeting
+  async function setGreeting() {
+    if (!greeting) return;
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input
+          onChange={(e) => setGreetingValue(e.target.value)}
+          placeholder="Set greeting"
+        ></input>
+      </header>
+    </div>
+  );
+}
+
+export default App;
